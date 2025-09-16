74aa025134b9c89ed8742a1596d4683e78ad400e
saiTeja <nedunoorisaiteja2108@gmail.com>
2025-09-16 07:54:34 +0530

    optionalCreation & OptionalAccess Methods were added

---
+++ Optional/OptionalAccessingValues.java
@@ -0,0 +1,57 @@
+// ---------Optional Accessing values methods------
+// 1.isPresent() -> Check if value Exists
+// 2. isEmpty() -> check if value is empty, Opposite to isPresent(java 11 +)
+// 3. get()     -> retrieve values,unsafe if empty
+// 4.orElse(defaultvalue) -> return or defaultvalue
+// 5.orElseGet(supplier) -> return value or lazy defaultvalue
+// 6.orElseThrow(supplier) -> throw exception if empty
+
+import java.util.Optional;
+
+public class OptionalAccessingValues {
+    public static void main(String[] args) {
+
+        Optional<String> opt1 = Optional.of("Spring");
+        Optional<String> opt2 = Optional.empty();
+
+        // ---isPresent() & get()--
+        // Syntax : boolean present = opt.isPresent();
+        // example:
+        if(opt1.isPresent())
+        {
+            System.out.println("---isPresent() && get() method example ---\n"+opt1.get());
+        }
+        //  ---orElse(defaultvalue)---
+
+        // Syntax : <T> value = opt.orElse(defaultvalue);
+        // example:
+
+        System.out.println(opt2.orElse("---orElse example---\n default value"));
+
+        // ----orElseGet(supplier)-----
+        // Syntax : <T> value = opt.orElseGet(() -> value);
+        // example:
+        System.out.println(opt2.orElseGet(()->"---orElseGet example---\n lazy default value"));
+
+        // ------orElseThrow(supplier)---
+        // Syntax : <T> value = opt.orElseThrow(() -> new exception("not found"))
+
+        try 
+        {
+            System.out.println("---orElseThrow()---");
+            System.out.println(opt2.orElseThrow(() -> new RuntimeException()));
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+// RealTime example
+//  in Service layer
+//  String email = UserRepository.findbyId(123)
+//                               .map(user :: getEmail).orElse("no-Email@example.com");
+        
+    }
+    
+    
+
+}
---
+++ Optional/OptionalCreation.java
@@ -0,0 +1,36 @@
+package Optional;
+
+import java.util.Optional;
+
+// Optional is a container that may be hold non null value or be empty
+// It mainly used or introduced to avoid nullpointerExceptions
+
+// Optional can be Created in 3 ways
+// Syntax:
+// 1. Optional<T> opt1 = Optional.of(value)         ----> used for non Null value only, 
+                                                     // Throws nullpointerException
+// 2.Optional<T> opt2 = Optional.ofNullable(value)  ---> used for values that can be null as well,
+                                                    //  return Optional Container if value is null
+// 3.Optional<T> opt3 = Optional.empty();           ---> Represent no value
+
+// example
+public class OptionalCreation {
+
+    public static void main(String[] args) {
+        Optional<String> opt1 = Optional.of("Java");
+        Optional<String> opt2 = Optional.ofNullable(null);
+        Optional<String> opt3 = Optional.empty();
+
+        System.out.println("---Optional.of example---\n" + opt1);
+        System.out.println("---Optional.ofNullable example---\n" + opt2);
+        System.out.println("---Optional.empty example---\n" + opt3);
+    }
+
+// RealTime example
+//   Used in repository Methods
+// Optional <user> userOptional = UserRepository.findById(123);
+// •	Optional.empty() is returned if no user found.
+
+
+    
+}
